1c4d37350c0df9ea4d3cf8f9d869ca74eace090a
Furkan Unal <ffurkanunal@hotmail.com>
2023-01-26 17:07:10 +0300

    v2

---
+++ src/C01.java
@@ -0,0 +1,7 @@
+public class C01 {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("cmd çok kullanışlı");
+        System.out.println("Java akıyor");
+    }
+}
